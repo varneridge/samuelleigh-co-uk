c69c12d63d9f682533b7420233a5d4a18b8f9242
Samuel Leigh <s.leigh1@lse.ac.uk>
2026-09-15 00:44:36 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import rehypeFigure from './src/plugins/rehype-figure.mjs';
 
 export default defineConfig({
   site: 'https://samuelleigh.co.uk',
-  markdown: { shikiConfig: { theme: 'github-light' } }
+  markdown: {
+    shikiConfig: { theme: 'github-light' },
+    rehypePlugins: [rehypeFigure]
+  }
 });
